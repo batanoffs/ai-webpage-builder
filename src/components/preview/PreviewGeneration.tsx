57170a5e4fdcfcfdb196ec34df2fe967de6ef49d
batanoffs <batanoff.s@protonmail.com>
2024-11-22 00:57:53 +0200

    removed

--- src/components/preview/PreviewGeneration.tsx
+++
@@ -1,9 +0,0 @@
-export const PreviewGeneration = () => {
-    return (
-        <aside className="h-[100dvh] w-[50%]">
-            <h1> Preview </h1>
-
-            <iframe srcDoc="" className="border rounded-md" />
-        </aside>
-    );
-};
