19d59da18ada5d85fd473c7d611aefd93c1d9687
batanoffs <batanoff.s@protonmail.com>
2024-11-22 01:00:38 +0200

    deprecated

--- src/utils/updateIframeContent.ts
+++ src/utils/updateIframeContent.ts
@@ -1,20 +1,22 @@
-/**
- * @param {HTMLIFrameElement} iframe
- * @param {{ html: string, css: string, js: string }} args
- * @returns {void}
- */
+// DEPRECATED
 
-type updateIframeContent = (iframe: HTMLIFrameElement, args: { html: string; css: string; js: string }) => void;
+// /**
+//  * @param {HTMLIFrameElement} iframe
+//  * @param {{ html: string, css: string, js: string }} args
+//  * @returns {void}
+//  */
 
-export const updateIframeContent: updateIframeContent = (iframe, { html, css, js }): void => {
-    const source = `
-      <html>
-        <head><style>${css}</style></head>
-        <body>
-          ${html}
-          <script>${js}</script>
-        </body>
-      </html>
-    `;
-    iframe.srcdoc = source;
-};
+// type updateIframeContent = (iframe: HTMLIFrameElement, args: { html: string; css: string; js: string }) => void;
+
+// export const updateIframeContent: updateIframeContent = (iframe, { html, css, js }): void => {
+//     const source = `
+//       <html>
+//         <head><style>${css}</style></head>
+//         <body>
+//           ${html}
+//           <script>${js}</script>
+//         </body>
+//       </html>
+//     `;
+//     iframe.srcdoc = source;
+// };
