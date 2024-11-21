67c48f040bb27facf09f3e82c6fe45e33a6fb295
batanoffs <batanoff.s@protonmail.com>
2024-11-22 00:58:54 +0200

    update constants

--- src/constants/instructions.ts
+++ src/constants/instructions.ts
@@ -1,11 +1,39 @@
-const SYSTEM_INSTRUCTIONS = `You are a helpful assistant that writes HTML, CSS and JavaScript code for building a webpage.
-Do not mention what tech stack you are using. 
+const SYSTEM_INSTRUCTIONS = `
+You are a helpful assistant that writes HTML, CSS and JavaScript code for building a webpage. 
 You will be given a prompt by the user, then you return a response which describes very shortly what you have done. 
-After that wrap your code in a code block to enclose the code. Do not add any other text or comments.`;
+After that wrap your new code in a code block combined with the existing code in previous responses if any.
+Do not add any other text or comments. Write down the css in style tags and the js in script tags. If needed use external images to represent the content.
+If the user provides follow up commands, change only the code or component related with his request and keep the rest the same.
+Code Generation Process:
+Always start with <!DOCTYPE html> <!DOCTYPE html>. Don't forget to add alt attributes to all images.
+When generating code, use the entire conversation history to guide your responses.
+Use the context and requirements provided in the initial prompt and subsequent updates to generate code that meets the user's needs.
+Use a consistent coding style and formatting throughout the generated code. Add comments for each main section`;
 
 const MODEL = 'gpt-4o';
 
+const API_INITIAL_STATE = [
+    {
+        role: 'system' as 'system',
+        content: [
+            {
+                type: 'text' as 'text',
+                text: SYSTEM_INSTRUCTIONS,
+            },
+        ],
+    },
+];
+
+const CHAT_INITIAL_STATE = [
+    {
+        id: 0,
+        role: 'bot' as 'bot',
+        message: 'Hello! I can help you build your webpage. What would you like to create?',
+    },
+];
+
 export const AI = {
-    SYSTEM_INSTRUCTIONS,
     MODEL,
+    API_INITIAL_STATE,
+    CHAT_INITIAL_STATE,
 };
