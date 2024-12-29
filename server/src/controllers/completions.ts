5dbd26ab81323ea59fca17a93512c700f7509118
batanoffs <batanoff.s@protonmail.com>
2024-12-29 20:33:44 +0200

    add controller for chat completions

---
+++ server/src/controllers/completions.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { openAiService } from '../service/completions';
+
+const getCompletions = async (req: Request, res: Response): Promise<void> => {
+	try {
+		// Get user message from request body
+		const { userMessage } = req.body;
+
+		// Call the completion service
+		const completions = await openAiService.generateText(userMessage);
+
+		// Check if completions were found
+		if (!completions) res.status(404).json({ message: 'No completions found' });
+
+		// Check if there was an error
+		if (completions && completions instanceof Error)
+			res.status(500).json({ message: `Error ${completions.message}` });
+
+		// Send the completion back to the client
+		res.status(200).json({ completions });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Internal server error' });
+	}
+};
+
+export { getCompletions };
