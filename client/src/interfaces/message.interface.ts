92c0e88b8bad9d55566fbe169f75fb5b3b8dd788
batanoffs <batanoff.s@protonmail.com>
2024-12-29 20:30:22 +0200

    update interface

--- client/src/interfaces/message.interface.ts
+++ client/src/interfaces/message.interface.ts
@@ -1,29 +1,6 @@
 interface ChatMessage {
-    id: number;
-    message: string;
-    role: 'user' | 'bot';
+    role: 'user' | 'assistant' | 'system';
+    content: string;
 }
 
 export type ChatMessages = Array<ChatMessage>;
-
-interface TextContent {
-    type: 'text';
-    text: string;
-}
-
-interface SystemMessage {
-    role: 'system';
-    content: TextContent[];
-}
-
-interface AssistantMessage {
-    role: 'assistant';
-    content: string;
-}
-
-interface UserMessage {
-    role: 'user';
-    content: string;
-}
-
-export type AiMessages = Array<SystemMessage | AssistantMessage | UserMessage>;
