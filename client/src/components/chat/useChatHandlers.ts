01698d992fee43c0f937452bd8429b0bfd6ccdab
batanoffs <batanoff.s@protonmail.com>
2024-12-29 20:28:11 +0200

    remove most of the logic for the service because it will be done in the server side, also update the hook accordingly

--- client/src/components/chat/useChatHandlers.ts
+++ client/src/components/chat/useChatHandlers.ts
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 import { aiService } from '../../services/openAI';
-import { AiMessages, ChatMessages } from '../../interfaces/message.interface';
-import { AI } from '../../constants/instructions';
+import { ChatMessages } from '../../interfaces/message.interface';
+import { initialChatMessage } from '../../utils/chat';
 
 type useChatProps = {
     setSourceCode: (sourceCode: string) => void;
     setCurrentTab: (tab: string) => void;
 };
 
-const apiContext: AiMessages = AI.API_INITIAL_STATE;
+//TODO update types 
 
 export const useChatHandlers = ({ setSourceCode, setCurrentTab }: useChatProps) => {
-    const [chatMessages, setChatMessages] = useState<ChatMessages>(AI.CHAT_INITIAL_STATE);
+    const [chatMessages, setChatMessages] = useState<ChatMessages>([initialChatMessage]);
     const [value, setValue] = useState<string>('');
 
     const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,67 +27,19 @@ export const useChatHandlers = ({ setSourceCode, setCurrentTab }: useChatProps) 
             const form = new FormData(event.target as HTMLFormElement);
             const { prompt } = Object.fromEntries(form);
 
-            if (typeof prompt !== 'string') throw new Error('Invalid prompt');
+            if (!prompt || typeof prompt !== 'string') throw new Error('Invalid prompt');
 
-            setChatMessages((prev) => [
-                ...prev,
-                {
-                    id: prev.length,
-                    role: 'user',
-                    message: prompt,
-                },
-            ]);
+            const response = await aiService.getSteamData(prompt);
 
-            apiContext.push({ role: 'user', content: `${prompt}` });
-
-            // -----------------------------------------------------------
-            // The service without streaming the data
-            // -----------------------------------------------------------
-
-            // const response = await aiService.getCompletions(apiContext);
-            // if (response.status !== 200) throw new Error('Request failed with status ' + response.status);
-            // const data = response.data;
-            // if (!data) throw new Error('No data received');
-            // const aiMessage = data.choices[0].message.content;
-            // const parsedAiMessage = JSON.parse(aiMessage);
-            // setChatMessages((prev) => [
-            //     ...prev,
-            //     {
-            //         id: prev.length,
-            //         role: 'bot',
-            //         message: parsedAiMessage.answer,
-            //     },
-            // ]);
-            // apiContext.push({ role: 'assistant', content: `${parsedAiMessage.code}` });
-            // setSourceCode(parsedAiMessage.code);
-
-            // -----------------------------------------------------------
-            // The service that streams the data
-            // -----------------------------------------------------------
-
-            const response = await aiService.getSteamData(apiContext, setSourceCode);
             if (!response || response === undefined) throw new Error('Request failed');
 
             const { data } = response;
 
             if (!data) throw new Error('No description or code received');
 
-            const destructureData = data.split('\nhtml\n');
-            const description = destructureData[0];
-            const code = destructureData[1];
-
-            setChatMessages((prev) => [
-                ...prev,
-                {
-                    id: prev.length,
-                    role: 'bot',
-                    message: description,
-                },
-            ]);
-
-            apiContext.push({ role: 'assistant', content: code });
-
             setCurrentTab('PREVIEW');
+
+            return { data };
         } catch (error) {
             if (error instanceof Error) {
                 console.log(error.message);
