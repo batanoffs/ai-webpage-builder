1d2b6adf198f1fd342c23e51354d10e47b687fc4
batanoffs <batanoff.s@protonmail.com>
2024-12-30 19:43:49 +0200

    add body parser dep to server and use it as middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import routeConfig from './config/routes';
-
+import bodyParser from 'body-parser';
 // Create a new express application instance
 const app = express();
 
@@ -8,6 +8,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 try {
+	// Set up the express app to parse JSON
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+
 	// Config the routes
 	routeConfig(app);
 
