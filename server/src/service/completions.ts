414a79b6ba2f8d4ea34630e9b8ee2261df37250e
batanoffs <batanoff.s@protonmail.com>
2024-12-30 20:14:49 +0200

    update rename service, add comments, import chatMessages array to save context and cast types. Add to do

--- server/src/service/completions.ts
+++ server/src/service/completions.ts
@@ -1,15 +1,29 @@
+/**
+ * Generates text based on the user's message using OpenAI's API.
+ *
+ * @param {string} userMessage - The message from the user to generate a response for.
+ * @returns {Promise<any>} The chat completion response from OpenAI or an error if the operation fails.
+ * @throws {Error} If the user message is invalid.
+ *
+ * @example
+ * const response = await generateText("Hello, how are you?");
+ * console.log(response);
+ */
+
 import OpenAI from 'openai';
-import { aiConfig } from '../utils/instructions';
+import { ChatCompletionMessage } from 'openai/resources';
+import { chatMessages, GPT_MODEL } from '../utils/instructions';
 
-export const generateText = async (userMessage: string) => {
+export const generateWebsite = async (userMessage: string) => {
 	try {
+		// Check if the user message is valid string or not
 		if (!userMessage || typeof userMessage !== 'string')
-			throw new Error('Invalid user message');
-		// Create messages array
-		const messages = [];
+			throw new Error(
+				`Invalid user message prompt. The type of the variable is ${typeof userMessage}`
+			);
 
-		// Add system and assistant messages to messages array
-		messages.push(aiConfig.system, aiConfig.assistant, {
+		// Push user message to messages array
+		chatMessages.push({
 			role: 'user',
 			content: userMessage,
 		});
@@ -19,21 +33,27 @@ export const generateText = async (userMessage: string) => {
 
 		// Get steam completions from OpenAI
 		const stream = await openai.beta.chat.completions.stream({
-			model: aiConfig.model,
-			// store: true, // TODO: Do i need to store?
+			model: GPT_MODEL,
 			stream: true,
-			messages, //TODO: Check why it needs user prop
+			messages: chatMessages as ChatCompletionMessage[],
+			// store: true, // TODO: Do I need to store?
 		});
 
 		// Listen for content events
 		stream.on('content', (delta, snapshot) => {
 			process.stdout.write(delta);
+			
+			//TODO check if delta is a string or object
+			console.log('delta steams:', delta);
 		});
-		// or, equivalently:
+
+		// Or loop through the stream like this:
 		// for await (const chunk of stream) {
 		// 		process.stdout.write(chunk.choices[0]?.delta?.content || '');
 		// }
-		// cancel the stream stream.controller.abort().then(() => console.log('Stream cancelled'));
+
+		// If want to cancel the stream use abort method:
+		// stream.controller.abort().then(() => console.log('Stream cancelled'));
 
 		// Get final chat completion
 		const chatCompletion = await stream.finalChatCompletion();
@@ -47,5 +67,5 @@ export const generateText = async (userMessage: string) => {
 };
 
 export const openAiService = {
-	generateText,
+	generateWebsite,
 };
