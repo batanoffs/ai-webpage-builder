2c572566cffc5811a88ae9f4468541f9fda039a7
batanoffs <batanoff.s@protonmail.com>
2024-12-30 19:42:28 +0200

    fix the correct vite env

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,6 +1,6 @@
 // TODO fix env for client
 
-export const baseURL = process.env.serverURL || 'http://localhost:3000/api/';
+export const baseURL = import.meta.env.VITE_SERVER_BASE_URL || 'http://localhost:3000/api/';
 
 export const API = {
     chat: baseURL + 'completions',
