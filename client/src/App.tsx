0b51e10716c9d98de681eebb0355d111b8ad96a9
batanoffs <batanoff.s@protonmail.com>
2025-02-04 16:06:34 +0200

    update layout to be more consistent

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import { ChatSection, AppLayout, PreviewSection } from './features';
+import { ChatHeader, Navigation } from './features/layout/components';
 
 export const App = () => {
     const [sourceCode, setSourceCode] = useState<string>('');
     const [currentTab, setCurrentTab] = useState<'PREVIEW' | 'CODE'>('PREVIEW');
 
     return (
-        <AppLayout>
-            <ChatSection setSourceCode={setSourceCode} setCurrentTab={setCurrentTab} />
-            <PreviewSection sourceCode={sourceCode} currentTab={currentTab} setCurrentTab={setCurrentTab} />
-        </AppLayout>
+        <AppLayout
+            headerLeft={<ChatHeader />}
+            headerRight={<Navigation currentTab={currentTab} setCurrentTab={setCurrentTab} />}
+            chat={<ChatSection setSourceCode={setSourceCode} setCurrentTab={setCurrentTab} />}
+            preview={<PreviewSection sourceCode={sourceCode} currentTab={currentTab} />}
+        />
     );
 };
--- client/src/features/layout/AppLayout.tsx
+++ client/src/features/layout/AppLayout.tsx
@@ -1,5 +1,23 @@
-import { PropsWithChildren } from 'react';
+import { ReactNode } from 'react';
 
-export const AppLayout = ({ children }: PropsWithChildren) => {
-    return <main className="flex">{children}</main>;
+type AppLayoutProps = {
+    headerLeft: ReactNode;
+    headerRight: ReactNode;
+    chat: ReactNode;
+    preview: ReactNode;
+};
+
+export const AppLayout = ({ headerLeft, headerRight, chat, preview }: AppLayoutProps) => {
+    return (
+        <main className="flex flex-col h-[100dvh] max-h-[100dvh] w-full">
+            <header className="flex border-b">
+                <div className="w-1/2 border-r">{headerLeft}</div>
+                <div className="w-1/2">{headerRight}</div>
+            </header>
+            <section className="flex flex-1 w-full bg-white">
+                <aside className="w-1/2 overflow-hidden">{chat}</aside>
+                <aside className="w-1/2 overflow-hidden">{preview}</aside>
+            </section>
+        </main>
+    );
 };
