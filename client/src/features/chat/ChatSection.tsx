c0187e7455929221d4503ed49c568701d7709d13
batanoffs <batanoff.s@protonmail.com>
2025-02-04 16:06:54 +0200

    refactor ChatSection layout and styles for improved readability

--- client/src/features/chat/ChatSection.tsx
+++ client/src/features/chat/ChatSection.tsx
@@ -8,30 +8,30 @@ type ChatSectionProps = {
 
 export const ChatSection = ({ setSourceCode, setCurrentTab }: ChatSectionProps) => {
     const { chatMessages, value, changeHandler, handleSubmit } = useChatHandlers({ setSourceCode, setCurrentTab });
+
+    const userChatStyles = 'bg-blue-500 text-white';
+    const aiChatStyles = 'border border-slate-200 bg-white';
+
     return (
-        <section className="flex sticky flex-col h-[100dvh] w-[50%] border-r-[1px]">
-            <div className="h-[5dvh] border-b-[1px] pl-4 flex items-center">
-                <h1 className="font-bold">AI Webpage Builder</h1>
-            </div>
-            <div className="flex flex-col gap-2 p-4 h-[90dvh] border-b-[1px] bg-gray-50 overflow-y-scroll no-scrollbar">
-                {chatMessages.map((entry: ChatMessage, index: number) => (
+        <section className="flex flex-col h-full w-full border-r">
+            <div className="flex-1 flex flex-col gap-2 p-4 bg-gray-50 overflow-y-auto">
+                {chatMessages.map((message: ChatMessage, index: number) => (
                     <p
-                        key={entry.content}
-                        className={`p-2 rounded-md w-fit min-w-fit 
-                            ${index % 2 !== 0 ? 'bg-blue-500 text-white' : 'border border-slate-200 bg-white'}
-                            `}
+                        key={message.content}
+                        className={`p-2 rounded-md w-fit min-w-fit ${index % 2 !== 0 ? userChatStyles : aiChatStyles}`}
                     >
-                        {entry.content}
+                        {message.content}
                     </p>
                 ))}
             </div>
 
-            <form className="flex h-[5dvh] gap-2 m-4" onSubmit={handleSubmit}>
+            <form className="flex h-18 gap-2 p-4" onSubmit={handleSubmit}>
                 <input
                     className="flex-1 p-4 resize-none border rounded-md border-neutral-500"
                     placeholder={'Type your instructions here...'}
                     type="text"
                     name="prompt"
+                    id="prompt"
                     draggable={false}
                     onChange={changeHandler}
                     value={value}
